a597de8b4e31d498568966ca54a58c180051dff0
abduakhads <160651255+abduakhads@users.noreply.github.com>
2025-04-02 23:15:46 +0200

    initial commit

---
+++ QuizProject/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace QuizProject
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.QuestionLabel = new System.Windows.Forms.Label();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.PrevBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // QuestionLabel
+            // 
+            this.QuestionLabel.AutoSize = true;
+            this.QuestionLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuestionLabel.ForeColor = System.Drawing.Color.AliceBlue;
+            this.QuestionLabel.Location = new System.Drawing.Point(34, 36);
+            this.QuestionLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.QuestionLabel.Name = "QuestionLabel";
+            this.QuestionLabel.Size = new System.Drawing.Size(86, 21);
+            this.QuestionLabel.TabIndex = 3;
+            this.QuestionLabel.Text = "Question?";
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.NextBtn.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NextBtn.Location = new System.Drawing.Point(168, 218);
+            this.NextBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(157, 24);
+            this.NextBtn.TabIndex = 6;
+            this.NextBtn.Text = "Next";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // PrevBtn
+            // 
+            this.PrevBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.PrevBtn.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PrevBtn.Location = new System.Drawing.Point(11, 218);
+            this.PrevBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.PrevBtn.Name = "PrevBtn";
+            this.PrevBtn.Size = new System.Drawing.Size(153, 24);
+            this.PrevBtn.TabIndex = 5;
+            this.PrevBtn.Text = "Previous";
+            this.PrevBtn.UseVisualStyleBackColor = true;
+            this.PrevBtn.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(336, 266);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.PrevBtn);
+            this.Controls.Add(this.QuestionLabel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(490, 305);
+            this.MinimumSize = new System.Drawing.Size(352, 305);
+            this.Name = "Form1";
+            this.Text = "Quiz App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label QuestionLabel;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Button PrevBtn;
+    }
+}
---
+++ QuizProject/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace QuizProject
+{
+    public class Question
+    {
+        public string Text { get; }
+        public string[] Answers { get; }
+        public int[] CorrectIndexes { get; }
+        public bool IsMultipleChoice { get; }
+
+        public Question(string text, string[] answers, int[] correctIndexes, bool isMultipleChoice)
+        {
+            Text = text;
+            Answers = answers;
+            CorrectIndexes = correctIndexes;
+            IsMultipleChoice = isMultipleChoice;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private int currentQuestionIndex = 0;
+        private List<Question> questions;
+        private List<Control> answerControls = new List<Control>();
+        private int correctAnswersCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadQuestions();
+            LoadQuestion();
+        }
+
+        private void LoadQuestions()
+        {
+            questions = new List<Question>
+            {
+                new Question("What is 5 + 7?", new string[] { "10", "11", "12" }, new int[] { 2 }, false),
+                new Question("Select even numbers:", new string[] { "2", "3", "5", "6" }, new int[] { 0, 3 }, true),
+                new Question("Capital of Italy?", new string[] { "Rome", "Berlin", "Madrid" }, new int[] { 0 }, false),
+                new Question("What is 4 x 4?", new string[] { "12", "14", "16" }, new int[] { 2 }, false),
+                new Question("Which are web browsers?", new string[] { "Chrome", "Java", "Firefox", "C#" }, new int[] { 0, 2 }, true),
+                new Question("What is 20 / 4?", new string[] { "4", "5", "6" }, new int[] { 1 }, false),
+                new Question("Which colors are in the CMYK model?", new string[] { "Cyan", "Magenta", "Yellow", "Red" }, new int[] { 0, 1, 2 }, true),
+                new Question("Smallest planet?", new string[] { "Mercury", "Venus", "Earth" }, new int[] { 0 }, false),
+                new Question("Who developed Java?", new string[] { "Oracle", "Sun Microsystems", "IBM" }, new int[] { 1 }, false),
+                new Question("Which are vegetables?", new string[] { "Carrot", "Tomato", "Cucumber", "Apple" }, new int[] { 0, 2 }, true)
+
+            };
+        }
+
+        private void LoadQuestion()
+        {
+            if (currentQuestionIndex >= questions.Count)
+            {
+                DisplaySummary();
+                return;
+            }
+
+            var question = questions[currentQuestionIndex];
+            QuestionLabel.Text = question.Text;
+
+            foreach (var control in answerControls)
+            {
+                Controls.Remove(control);
+                control.Dispose();
+            }
+            answerControls.Clear();
+
+            int yPosition = QuestionLabel.Bottom + 10;
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Control answerControl = question.IsMultipleChoice ? (Control)new CheckBox() : new RadioButton();
+                answerControl.Text = question.Answers[i];
+                answerControl.Tag = question.CorrectIndexes.Contains(i) ? "1" : "0";
+                answerControl.Left = QuestionLabel.Left;
+                answerControl.Top = yPosition;
+                answerControl.AutoSize = true;
+                answerControl.ForeColor = System.Drawing.Color.AliceBlue;
+                Controls.Add(answerControl);
+                answerControls.Add(answerControl);
+                yPosition += 30;
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            bool isCorrect = true;
+            foreach (var control in answerControls)
+            {
+                bool isChecked = (control is RadioButton rb && rb.Checked) || (control is CheckBox cb && cb.Checked);
+                bool shouldBeChecked = control.Tag.ToString() == "1";
+
+                if (isChecked != shouldBeChecked)
+                {
+                    isCorrect = false;
+                    break;
+                }
+            }
+            if (isCorrect)
+                correctAnswersCount++;
+
+            if (currentQuestionIndex < questions.Count - 1)
+            {
+                currentQuestionIndex++;
+                LoadQuestion();
+            }
+            else
+            {
+                DisplaySummary();
+            }
+        }
+
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            if (currentQuestionIndex > 0)
+            {
+                currentQuestionIndex--;
+                LoadQuestion();
+            }
+        }
+
+        private void DisplaySummary()
+        {
+            QuestionLabel.Text = "Quiz Completed!";
+            foreach (var control in answerControls)
+            {
+                Controls.Remove(control);
+                control.Dispose();
+            }
+            answerControls.Clear();
+
+            Label summaryLabel = new Label();
+            summaryLabel.Text = $"You answered {correctAnswersCount} out of {questions.Count} questions correctly.";
+            summaryLabel.Left = QuestionLabel.Left;
+            summaryLabel.Top = QuestionLabel.Bottom + 20;
+            summaryLabel.AutoSize = true;
+            summaryLabel.BackColor = System.Drawing.Color.AliceBlue;
+            Controls.Add(summaryLabel);
+
+            NextBtn.Text = "Close";
+            NextBtn.Click += (s, e) => this.Close();
+            PrevBtn.Visible = false;
+        }
+    }
+
+
+}
+
+
